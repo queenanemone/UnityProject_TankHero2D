32026512ea4f6d696ada8f6886a8e50963842571
queenanemone <shinhs9902@gmail.com>
2023-02-12 18:49:02 +0900

    MODIFY : Modified tag system.

--- BulletDestroy.cs
+++ BulletDestroy.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class BulletDestroy : MonoBehaviour
 {
-    [SerializeField] ParticleSystem particle;
+    [SerializeField] ParticleSystem playerParticle;
+    [SerializeField] ParticleSystem enemyParticle;
+    ParticleSystem particle;
 
     Quaternion pos;
     // Start is called before the first frame update
@@ -17,11 +19,19 @@ public class BulletDestroy : MonoBehaviour
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if (other.tag == "Bullet"){
+        if (other.tag == "Player_Bullet" || other.tag == "Enemy_Bullet"){
+            if (other.tag == "Player_Bullet")
+            {
+                particle = playerParticle;
+            }
+            else
+            {
+                particle = enemyParticle;
+            }
             particle.transform.position = other.transform.position;
             particle.transform.rotation = pos;
             particle.Play();
-            Debug.Log("asdf");
+            Debug.Log("Boom!");
             Destroy(other.gameObject);
         }
     }
