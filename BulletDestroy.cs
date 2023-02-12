b6c34d9879bd8dd3cf56b67db11fc01ab720649c
queenanemone <shinhs9902@gmail.com>
2023-02-12 15:05:58 +0900

    ADD : Added particle system

--- BulletDestroy.cs
+++ BulletDestroy.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class BulletDestroy : MonoBehaviour
 {
+    [SerializeField] ParticleSystem particle;
+
+    Quaternion pos;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        pos = this.gameObject.transform.rotation;
     }
 
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Bullet"){
+            particle.transform.position = other.transform.position;
+            particle.transform.rotation = pos;
+            particle.Play();
             Debug.Log("asdf");
             Destroy(other.gameObject);
         }
